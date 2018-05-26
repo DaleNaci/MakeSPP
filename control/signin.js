322e8e9fac6720eb81e76fc8ebb73e2ce08865e6
Dale Nacianceno <dale.nash01@gmail.com>
2018-05-26 17:10:46 -0400

    Rewrote login function

--- control/signin.js
+++ control/signin.js
@@ -12,22 +12,11 @@ let data;
 
 
 function login() {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('controller').style.display = 'block';
+        console.log(firebase.auth().currentUser);
+    })
     
     var firebaseUserRef = firebase.database().ref("games/0/"+firebase.auth().currentUser.uid);
         
