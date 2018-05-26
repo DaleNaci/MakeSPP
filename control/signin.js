5226c0d9e0baf5a77859a6063a9cd38cdc2924bd
Dale Nacianceno <dale.nash01@gmail.com>
2018-05-26 17:09:25 -0400

    Updated signin.js

--- control/signin.js
+++ control/signin.js
@@ -10,10 +10,29 @@ firebase.initializeApp(config);
 let provider = new firebase.auth.GoogleAuthProvider();
 let data;
 
+
 function login() {
     firebase.auth().signInWithPopup(provider).then(function(result) {
-        document.getElementById('login').style.display = 'none';
-        document.getElementById('controller').style.display = 'block';
-        console.log(firebase.auth().currentUser);
-    })
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    
+    var firebaseUserRef = firebase.database().ref("games/0/"+firebase.auth().currentUser.uid);
+        
+    firebaseUserRef.on('value', snap => {
+        color = snap.child("color").val();
+    });
+    $('body').css('background-color', color);
 }
